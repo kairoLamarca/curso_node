66b0d4c589cae7e6a865a624e52c5908d4d513ac
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-09-27 23:26:40 -0300

    Valores MySQL
    Retornando valores do MySql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,16 +1,22 @@
 module.exports = function(app){
 
-	var mysql = require('mysql');
-
-	//os valores são passados em uma estrutura json
-	var connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '1234',
-		database: 'portal_noticias';
-	});
-
 	app.get('/noticias', function(req, res){
-		res.render("noticias/noticias");
+		
+		var mysql = require('mysql');
+
+		//os valores são passados em uma estrutura json
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '1234',
+			database: 'portal_noticias'
+		});
+
+		//connection.query(<sql>, <func callback>)
+		connection.query('select * from noticias', function(error, result){
+			res.send(result);
+		});
+	
+		//res.render("noticias/noticias");
 	});
 };

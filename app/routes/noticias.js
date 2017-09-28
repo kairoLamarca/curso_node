21cb5064642bf98578446555aac4014b99d2657d
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-09-27 23:09:28 -0300

    Connection Mysql
    modulo do mysql, conexão e alguns comandos mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,4 +1,15 @@
 module.exports = function(app){
+
+	var mysql = require('mysql');
+
+	//os valores são passados em uma estrutura json
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '1234',
+		database: 'portal_noticias';
+	});
+
 	app.get('/noticias', function(req, res){
 		res.render("noticias/noticias");
 	});

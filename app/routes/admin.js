ef1d23d83d204b88f537ee09898edf3b7f6a6fc2
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-10-02 23:12:19 -0300

    Tela de inclusão e body parser
    implementando tela de inclusão e body parser

---
+++ app/routes/admin.js
@@ -0,0 +1,11 @@
+module.exports = function(app){
+	app.get('/formulario_inclusao_noticia', function(req, res){
+		res.render("admin/form_add_noticia");
+	});
+
+	app.post('/noticias/salvar', function(req, res){
+		//Quando enviamos informação via post, o express popula a proprieda body do request --body parser
+		var noticias = req.body;
+		res.send(noticias);
+	});
+}
--- config/server.js
+++ config/server.js
@@ -1,10 +1,14 @@
 var express = require('express');//o express retorna uma função
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 //var msg = require('./mod_teste');
 var app = express();
 app.set('view engine', 'ejs');//Configurar motor de geração de views para o EJS
 app.set('views', './app/views');//configura onde está as views
+
+//middleware
+app.use(bodyParser.urlencoded({extended: true}));//permite uma tratativa mais completa, o json da url
 
 //consign scan a routes e inclui no app
 consign()

29a8c5c349c74d95282da81d61736f1eb4afe50e
Kairo Lamarca <lamarca.kairo@gmail.com>
2017-10-05 22:22:43 -0300

    Salvar noticia
    implementado método de salvar noticia por post

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,20 @@
-module.exports = function(app){
-	app.get('/formulario_inclusao_noticia', function(req, res){
+module.exports = function(application){
+	application.get('/formulario_inclusao_noticia', function(req, res){
 		res.render("admin/form_add_noticia");
 	});
 
-	app.post('/noticias/salvar', function(req, res){
+	application.post('/noticias/salvar', function(req, res){
 		//Quando enviamos informação via post, o express popula a proprieda body do request --body parser
-		var noticias = req.body;
-		res.send(noticias);
+		var noticia = req.body;
+
+		var connection = application.config.dbConnection();//dbConnection vem no app por causa do consign
+		var noticiasModel = application.app.models.noticiasModel;
+
+		//connection.query(<sql>, <func callback>)
+		noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+			//passa o retorno como um json
+			//res.render("noticias/noticias", {noticias: result});
+			res.redirect('/noticias');//redirect para que não ocorra de reenviar o form com o F5 pois foi em post
+		});
 	});
 }

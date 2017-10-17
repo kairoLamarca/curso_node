197dbee2be3729b347c3bb19e6db843083ffa3af
kairolamarca <lamarca.kairo@gmail.com>
2017-10-16 23:55:18 -0200

    correção da rota de noticia e noticias
    correção da rota de noticia e noticias

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,21 +1,33 @@
 //var dbConnection = require('../../config/dbConnection');//recuperando a função
 
-module.exports = function(application){
+module.exports = function (application) {
 
 	//var connection = dbConnection();//executando a função	
 
-	application.get('/noticias', function(req, res){
+	application.get('/noticias', function (req, res) {
 
 		var connection = application.config.dbConnection();//dbConnection vem no app por causa do consign
 		var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
 		//connection.query(<sql>, <func callback>)
-		noticiasModel.getNoticias(function(error, result){
+		noticiasModel.getNoticias(function (error, result) {
 			//passa o retorno como um json
-			res.render("noticias/noticias", {noticias: result});
+			res.render("noticias/noticias", { noticias: result });
 		});
-		
-	
+
+
 		//res.render("noticias/noticias");
+	});
+
+	application.get('/noticia', function (req, res) {
+
+		var connection = application.config.dbConnection();//dbConnection vem no app por causa do consign
+		var noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+		//connection.query(<sql>, <func callback>)
+		noticiasModel.getNoticia(function (error, result) {
+			//passa o retorno como um json
+			res.render("noticias/noticia", { noticia: result });
+		});
 	});
 };

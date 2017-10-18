1f10ae86ddd0a460fb26ce956471298db00d1d56
kairolamarca <lamarca.kairo@gmail.com>
2017-10-18 00:12:03 -0200

    noticia por id
    codificação para passar id de noticia e link da home

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -13,8 +13,10 @@ module.exports.noticia = function(application, req, res){
     var connection = application.config.dbConnection();//dbConnection vem no app por causa do consign
     var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
+    var id_noticia = req.query;
+
     //connection.query(<sql>, <func callback>)
-    noticiasModel.getNoticia(function (error, result) {
+    noticiasModel.getNoticia(id_noticia, function (error, result) {
         //passa o retorno como um json
         res.render("noticias/noticia", { noticia: result });
     });

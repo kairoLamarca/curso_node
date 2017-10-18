35ad1f6f1a6aa96230c21ea08db0dbb46a35c9c6
kairolamarca <lamarca.kairo@gmail.com>
2017-10-17 22:26:04 -0200

    app static e index
    inclusão de middleware express static no server para criar a pasta public static e alteração no layout da index

--- config/server.js
+++ config/server.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');//Configurar motor de geração de views para o EJ
 app.set('views', './app/views');//configura onde está as views
 
 //middleware
+app.use(express.static('./app/public'));//tudo que esta nesta pasta, fica como se estivesse na raiz pra ser acessado dentro da view sem um caminho grande
 app.use(bodyParser.urlencoded({extended: true}));//permite uma tratativa mais completa, o json da url
 app.use(expressValidator());
 

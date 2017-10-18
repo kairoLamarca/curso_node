5959f4bba7eddd05ae992f5491c9f83980513998
kairolamarca <lamarca.kairo@gmail.com>
2017-10-17 22:56:46 -0200

    home
    implemetaão da lógica de trazer as ultimas noticias na home

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,13 @@
 module.exports.index = function (application, req, res){
-    res.render("home/index");
+    
+    let connection = application.config.dbConnection();//dbConnection vem no app por causa do consign
+    let noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+    //callback espera pelo menos dois parametros, error e result
+    noticiasModel.get5UltimasNoticias((error, result) =>{
+        console.log(result);
+        res.render("home/index", {noticias: result});
+    });
+
+    
 }
